cd072c898ca0c62ffc012a3db25fdcf6a769d5b3
Ryan Gallaway <rgallaway@gatech.edu>
2018-10-09 20:55:07 -0400

    changed strings to bytes, transmission now seemingly works

--- Playground/mqtt-mpy/mqttconverter.py
+++ Playground/mqtt-mpy/mqttconverter.py
@@ -9,24 +9,30 @@ MQTT_PORT = 1883
 try:
     c = MQTTClient('hytech_car', MQTT_SERVER, MQTT_PORT)
     c.connect()
+    print('connected to MQTT broker')
+    c.publish('hytech_car/telemetry', 'Xbee Connected')
     #c.subscribe('hytech_car/telemetry')
 except OSError as e:
     c.disconnect()
 
 def read_uart():
-    incomingFrame = ''
+    incomingFrame = b''
     while True:
-        data = sys.stdin.read(1)
-        if len(data) > 0:
-            incomingFrame += binascii.hexlify(data, sep=' ') + ' '
-            if ('00' in incomingFrame):
-                frame = incomingFrame[0:incomingFrame.find("00")]
-                timestamp = str(time.localtime())
+        data = sys.stdin.buffer.read(-1)
+        if data is not None:
+            incomingFrame += binascii.hexlify(data, b' ')
+            print(incomingFrame)
+            if (b'00' in incomingFrame):
+                frame = incomingFrame[0:incomingFrame.find(b'00')]
+                timestamp = str.encode(str(time.localtime()))
+                print('sending message...')
+                print(timestamp, frame)
                 send_mqtt(timestamp, frame)
-                incomingFrame = incomingFrame[incomingFrame.find('00 ') + 3:]
+                incomingFrame = incomingFrame[incomingFrame.find(b'00 ') + 3:]
 
 def send_mqtt(timestamp, data):
-    msg = timestamp + ',' + data
+    msg = timestamp + b',' + data
+    print('sent message')
     print(msg)
     c.publish('hytech_car/telemetry', msg)
 

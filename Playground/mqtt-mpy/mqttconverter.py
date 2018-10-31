aca208bf14dfc544cfaa645f71740959f9ce4cbf
Ryan Gallaway <rgallaway@gatech.edu>
2018-10-30 23:23:33 -0400

    added length check before sending message

--- Playground/mqtt-mpy/mqttconverter.py
+++ Playground/mqtt-mpy/mqttconverter.py
@@ -3,8 +3,8 @@ import binascii
 import time
 import sys
 MESSAGE_LENGTH = 15
-MQTT_SERVER = 'test.mosquitto.org'
-MQTT_PORT = 1883
+MQTT_SERVER = 'iot.eclipse.org'     # MQTT broker hostname
+MQTT_PORT = 1883                    # MQTT broker port (non-SSL)
 try:
     c = MQTTClient('hytech_car', MQTT_SERVER, MQTT_PORT)
     c.connect()
@@ -19,16 +19,17 @@ def read_uart():
         data = sys.stdin.buffer.read(-1)
         if data is not None:
             incomingFrame += data
-            print(binascii.hexlify(incomingFrame))
+            print(binascii.hexlify(incomingFrame)) # for verification purposes
             while b'\x00' in incomingFrame:
                 end_index = incomingFrame.find(b'\x00')
-                print('First byte:', incomingFrame[0])
-                print('End index:', end_index)
-                frame = incomingFrame[0:end_index]
-                timestamp = str.encode(str(time.time()))        # epoch time
-                print('sending message...', type(frame))
-                print(timestamp, binascii.hexlify(frame))
-                send_mqtt(timestamp, frame)
+                if end_index >= 16: # only send complete messages to save data
+                    print('First byte:', incomingFrame[0])
+                    print('End index:', end_index)
+                    frame = incomingFrame[0:end_index + 1]
+                    timestamp = str.encode(str(time.time()))        # epoch time
+                    print('sending message...', type(frame))
+                    print(timestamp, binascii.hexlify(frame))
+                    send_mqtt(timestamp, frame)
                 incomingFrame = incomingFrame[end_index + 1:]
 def send_mqtt(timestamp, data):
     msg = timestamp + b',' + data
